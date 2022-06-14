d3de94e2c6f63497d537365fcc9959174e6a87e2
dhanush <vellasiridhanush@gmail.com>
2022-06-14 16:55:13 +0530

    added CURD operation  for cart section

---
+++ routes/product.js
@@ -0,0 +1,86 @@
+const { default: mongoose } = require("mongoose");
+// const { findByIdAndDelete, find } = require("../models/Product");
+const Product = require("../models/Product");
+const {
+  verifyToken,
+  verifyTokenAndAdmin,
+  verifyTokenAndAuthorisation,
+} = require("./verifyToken");
+const router = require("express").Router();
+
+router.post("/", verifyTokenAndAdmin, async (req, res) => {
+  const newProduct = new Product(req.body);
+  console.log(req.body);
+
+
+
+  try {
+    const savedProduct  = await newProduct.save();
+    res.status(200).json(savedProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.put("/:id", verifyToken, async (req, res) => {
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedProduct);
+  } catch (err) {
+    //refer here
+    // return res.status(500).json(err);
+    // console.log(err);
+  }
+});
+
+router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    await Product.findByIdAndDelete(req.params.id);
+    res.status(200).json("Successfully Product Deleted");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//Get Product
+
+router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    res.status(200).json(product);
+  } catch (err) {
+    res.status.json(err);
+  }
+});
+
+//Get All Products
+
+router.get("/", async (req, res) => {
+  const qNew = req.params.new;
+  const qCategory = req.params.category;
+  try {
+    let products;
+    if (qNew) {
+      products = await Product.find().sort({ createdAt: -1 }).limit(1);
+    } else if(qCategory){
+      products = await Product.find({
+        categories:{
+          $in:[qCategory]
+        }
+      });
+    }else{
+      products=await Product.find();
+    }
+    // const product = await find();
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+module.exports = router;
