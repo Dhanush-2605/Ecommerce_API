b6327f63073918057b24e7924322f0fa1e8133f2
dhanush <vellasiridhanush@gmail.com>
2022-08-16 22:26:23 +0530

    minor changes

--- routes/twilio.js
+++ routes/twilio.js
@@ -6,9 +6,8 @@ const { verifyTokenAndAdmin } = require('./verifyToken');
 router.post("/",verifyTokenAndAdmin,async(req,res)=>{
     console.log(req.body);
     // const accountSid = process.env.TWILIO_ACCOUNT_SID;
-    const accountSid="ACa4af5937ddf2cdb2d990539dafaf7904"
+ 
     // const authToken = process.env.TWILIO_AUTH_TOKEN;
-    const authToken="5fdb885c07ef2ec1402f1c3ddfd4deff"
     const client = require('twilio')(accountSid, authToken);
     client.messages
     .create({
